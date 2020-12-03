fda16dbcaacd76b6ebe770f83945d2bff807ced0
Guille <guiller22>
2020-12-03 20:41:30 +0100

    s

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DNS','mysql:dbname=conciertos;host=localhost');  
+define('DNS','mysql:dbname=conciertos;host=node29152-env-2826747.es-1.axarnet.cloud');  
 define('USERNAME','root');  
 define('PASSWORD','toor');
 ?>
